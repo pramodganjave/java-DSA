63c10ca39457435cb25ad832e54b0e6c1a7466de
pramodganjave <[ganjavepramod64@gmail.com]>
2024-08-01 18:53:46 +0530

    printlargestofthreeno

---
+++ printlargestofthreeno.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class printlargestofthreeno {
+    public static void main(String args[]) {
+        int A = 1, B = 3, C = 6;
+        if ((A >= B) && (A >= C)) {
+            System.out.println("Leargest is A");
+        } else if (B >= C) {
+            System.out.println("Largest is B");
+
+        } else {
+            System.out.println("Largest is C ");
+
+        }
+
+    }
+}
